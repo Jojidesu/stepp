79958d604f91c423914e3e67d1c19215747baacc
Giorgy Gunawan <giorgy.gunawan@foodpanda.com>
2021-10-17 20:18:46 +0800

    init project

---
+++ stepp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stepp
+//
+//  Created by Giorgy null on 17/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
